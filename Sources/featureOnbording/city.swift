763ed1dd9cf84ab887adfad77669425ef9f301a9
sandipermata <45228664+sandipermata@users.noreply.github.com>
2019-12-05 13:02:11 +0700

    city

---
+++ Sources/featureOnbording/city.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sandi Permata on 05.12.19.
+//
+
+import Foundation
+import Alamofire
+
+struct City {
+    let city_name: String
+    let city_code: String
+}
